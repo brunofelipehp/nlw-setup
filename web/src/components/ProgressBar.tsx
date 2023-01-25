e4e20aef94673392c1e1398f5b79ebd896feb8bf
Bruno Felipe <brunofelipehp8@gmail.com>
2023-01-24 23:48:27 -0300

    finalizing the web and mobile layout

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgressbarProps {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressbarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuemin={props.progress}
+        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        style={{
+          width: `${props.progress}%`
+        }}
+      ></div>
+    </div>
+  )
+}
